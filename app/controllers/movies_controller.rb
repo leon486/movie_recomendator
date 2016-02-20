989fd26bce50eca12db9703fb7e553628bb74ece
Leon <leon486@gmail.com>
2016-02-19 22:32:08 -0500

    created the initial form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,5 +4,14 @@ class MoviesController < ApplicationController
   end
 
   def add_movie
+    m = Movie.find_or_initialize_by(title:params[:title],year:params[:year])
+    m.title = params[:title]
+    m.year = params[:year]
+    m.release_date = params[:release_date]
+    m.save
+  end
+  
+  def delete
+    Movie.delete(params[:id]) 
   end
 end
