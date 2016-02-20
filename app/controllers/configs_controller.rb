6c0b4d3554614666a2653d0c17c993553c540673
Leon <leon486@gmail.com>
2016-02-20 10:09:02 -0500

    finished configuration form '

--- app/controllers/configs_controller.rb
+++ app/controllers/configs_controller.rb
@@ -12,5 +12,21 @@
 
 class ConfigsController < ApplicationController
   def save
+    c_notification_status    = Config.find_or_initialize_by(parameter:'notification_status')
+    c_time      = Config.find_or_initialize_by(parameter:'time')
+    c_time_type = Config.find_or_initialize_by(parameter:'time_type')
+    c_emails    = Config.find_or_initialize_by(parameter:'emails')
+    
+    c_notification_status.value = params[:notification_status]
+    c_notification_status.save
+    
+    c_time.value = params[:time]
+    c_time.save
+    
+    c_time_type.value = params[:time_type]
+    c_time_type.save
+    
+    c_emails.value = params[:emails]
+    c_emails.save
   end
 end
